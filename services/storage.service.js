1a363326b01ab2f6865f7f33a3b0fd1dc5dec6e6
Aleksey <alekseytim@mail.ru>
2022-08-29 08:10:09 +0300

    feat: работа с файловой системой

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,9 +1,36 @@
 import { homedir } from 'os';
 import { join } from 'path';
+import { promises } from 'fs';
 
 const filePath = join(homedir(), 'weather-data.json');
 
-const saveKeyValue = (key, value) => {
-}
+const saveKeyValue = async (key, value) => {
+  let data = {};
 
-export { saveKeyValue };
+  if(await isExist(filePath)){
+    const file = promises.readFile(filePath);
+    data = JSON.parse(file);
+  }
+  data[key] = value;
+  await promises.writeFile(filePath, JSON.stringify(data))
+};
+
+const getKeyValue = async (key) => {
+  if(await isExist(filePath)){
+    const file = promises.readFile(filePath);
+    const data = JSON.parse(file);
+    return data[key];
+  }
+  return undefined;
+};
+
+const isExist = async (path) => {
+  try {
+    await promises.stat();
+    return true;
+  } catch (error) {
+    return false
+  }
+};
+
+export { saveKeyValue, getKeyValue };
